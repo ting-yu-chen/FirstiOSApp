f788719f0683c5d8764f8094df96b610822c9f21
tinchen <tinchen@student.ethz.ch>
2021-02-16 10:48:47 +0100

    Initial Commit

---
+++ FirstiOSApp/FirstiOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstiOSAppApp.swift
+//  FirstiOSApp
+//
+//  Created by TingyuChen on 2021/2/16.
+//
+
+import SwiftUI
+
+@main
+struct FirstiOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
